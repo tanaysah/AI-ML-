d292192a0d83397d3357fc96d5b13ca62d4085d3
Tanay Sah <sah_cse@hotmail.com>
2026-01-28 10:49:44 +0530

    Add files via upload

---
+++ aiml2.py
@@ -0,0 +1,12 @@
+#given two number write a lambda function to return A if a>b and B if b>a.
+
+a = int(input("first number: "))
+b = int(input("second number: "))
+
+g = lambda a, b: a \
+    if a > b \
+    else b
+
+print("Greater number is:", g(a, b))
+
+
